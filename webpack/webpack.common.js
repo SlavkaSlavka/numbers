3f7593ab952cf9d1799d0bf4302f9d4937d0b912
Aruna <myruala@gmail.com>
2020-10-31 11:29:23 +0200

    styles import fixes

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -11,8 +11,8 @@ const dest = Path.join(__dirname, '../dist');
 
 module.exports = {
     entry: {
-        app: Path.resolve(__dirname, '../src/scripts/index'),
         vendors: Path.resolve(__dirname, '../src/scripts/vendors/index'),
+        app: Path.resolve(__dirname, '../src/scripts/index'),
     },
     output: {
         path: dest,
